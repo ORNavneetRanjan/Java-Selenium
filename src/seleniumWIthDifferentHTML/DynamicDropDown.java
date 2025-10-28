7c3cc44d71d49b618cade53cb821ba25c23785f3
ORNavneetRanjan <navneet.ranjan274057@gmail.com>
2025-10-28 08:01:12 +0530

    dynamic dropdown

--- src/seleniumWIthDifferentHTML/DynamicDropDown.java
+++ src/seleniumWIthDifferentHTML/DynamicDropDown.java
@@ -3,15 +3,29 @@ package seleniumWIthDifferentHTML;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
 
 public class DynamicDropDown {
-	public static void main(String... args) throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-		driver.findElement(By.className("left")).click();
-		Thread.sleep(1000);
-		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.cssSelector("a[@value = 'AMD'")).click();
-	}
+    public static void main(String... args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.manage().window().maximize();
+
+
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+       
+
+        driver.findElement(By.xpath("//a[@value='AMD']")).click();
+        
+        // Using parent child relationship of x path instead of hard coding index
+        
+        //driver.findElement(By.xpath("(//a[@value='BHO'])[2]")).click();
+        
+        driver.findElement(By.xpath("//div[@id = 'glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='BKK']")).click();
+
+        System.out.println("Successfully selected FROM and TO cities.");
+        Thread.sleep(5000);
+        driver.quit();
+    }
 }
