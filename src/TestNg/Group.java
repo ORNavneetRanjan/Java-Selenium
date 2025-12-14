6e6574eb14ccd1b7830aa4a3cd11eb843bd20e15
ORNavneetRanjan <ar-navneet.d.kumar@rakuten.com>
2025-12-14 16:47:39 +0530

    TestNg Listners

--- src/TestNg/Group.java
+++ src/TestNg/Group.java
@@ -1,5 +1,6 @@
 package TestNg;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -18,8 +19,32 @@ public class Group {
         System.out.println("\nData received");
     }
 
-    @Test(groups = {"Navneet"}, dependsOnMethods = {"bringData"})
+    @Test(groups = {"Navneet"}, dependsOnMethods = {"bringData"}, timeOut = 4000)
     public void algorithm(){
         System.out.println("Applying algorithms on the data we got.");
+    }
+
+    @DataProvider(name = "data")
+    public String[][] getData(){
+        String[][] arr = new String[3][2];
+        //first set
+        arr[0][0] = "hi@hello21.com";
+        arr[0][1] = "password1";
+
+        //second set
+        arr[1][0] = "hi@hello1.com";
+        arr[1][1] = "password2";
+
+        //third set
+        arr[2][0] = "hi@hello2.com";
+        arr[2][1] = "password3";
+
+        return arr;
+    }
+
+    @Test(dataProvider = "data")
+    private void display(String id, String pass){
+        System.out.println("ID: " + id);
+        System.out.println("PASSWORD: " + pass);
     }
 }
